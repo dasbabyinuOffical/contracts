55465dde1e25e3d5f95cf29120c620e7b9aad196
KevenNock <KevenNock@gmail.com>
2022-12-06 15:19:07 +0800

    add reward

---
+++ src/Reward.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.10;
+
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract Reward{
+    function deposit() external{
+
+    }
+
+}
